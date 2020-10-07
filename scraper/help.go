2f188e5c1b9461ebc3444b74595334924378b78d
Burak Sekili <buraksekili@gmail.com>
2020-10-07 20:43:35 +0300

    Added help flag

--- scraper/help.go
+++ scraper/help.go
@@ -1,1 +1,15 @@
 package scraper
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"io"
+)
+
+func PrintHelp(w io.Writer) {
+	name := "------ cli-news ------:\n "
+	help := "cli-news is a Hacker news scraper to display popular headlines of Hacker news without using external API to fetch popular headlines.\n\n" +
+		"-h, --help boolean\n\tPrints this message.\n" +
+		"<C-c> to quit program.\n"
+	fmt.Fprintf(w, "%s%s\n", color.GreenString(name), help)
+}
